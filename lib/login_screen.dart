709e80487162b7bf55ba3f0d0a4c56f28c43828f
mehrabbozorgi <mehrabbozorgi.business@gmail.com>
2025-02-03 13:27:35 +0330

    flutter nike shoe store

---
+++ lib/login_screen.dart
@@ -0,0 +1,152 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool textStatus = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Color(0xffF5F6FF),
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 25),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            spacing: 10,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Login screen',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'welcome back nike store',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14,
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.number, // کیبورد عددی
+                inputFormatters: [FilteringTextInputFormatter.digitsOnly], // فقط عدد مجاز است
+                decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  // hintText: 'phone number',
+                  hintText: 'phone number',
+                  hintStyle: TextStyle(
+                    color: Color(0xFFB3B3B3),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF2D96FF), width: 2),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Color(0xFFD2D2D2)),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              TextField(
+                keyboardType: TextInputType.visiblePassword,
+                obscureText: textStatus,
+                obscuringCharacter: '*',
+                decoration: InputDecoration(
+                  // prefixIcon: Icon(Icons.visibility),
+                  suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        if (textStatus) {
+                          textStatus = false;
+                        } else {
+                          textStatus = true;
+                        }
+                      });
+                    },
+                    ///if true = visible , else = invisible
+                    icon: textStatus ? Icon(Icons.visibility_outlined) : Icon(Icons.visibility_off_outlined),
+                  ),
+
+                  fillColor: Colors.white,
+                  filled: true,
+                  hintText: 'password',
+                  hintStyle: TextStyle(
+                    color: Color(0xFFB3B3B3),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Color(0xFF2D96FF), width: 2),
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: const BorderSide(color: Color(0xFFD2D2D2)),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Color(0xFF2D96FF),
+                ),
+                onPressed: () {},
+                child: Text('Forget password?'),
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Color(0xFF2D96FF),
+                    foregroundColor: Color(0xFFffffff),
+                    fixedSize: Size(double.maxFinite, 45),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(7.5))),
+                onPressed: () {},
+                child: Text('Login'),
+              ),
+
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Don’t have account?!'),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Color(0xFF2D96FF),
+                    ),
+                    onPressed: () {},
+                    child: Text('Create account'),
+                  ),
+                ],
+              ),
+
+//               ElevatedButton(
+//                 onPressed: () {
+//                   // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ),);
+//
+//
+//                   // Navigator.of(context).push(
+//                   //   MaterialPageRoute(
+//                   //     builder: (context) => LoginScreen(),
+//                   //   ),
+//                   // );
+//
+// // Navigator.of(context).pop();
+//
+//
+//                 },
+//                 child: Text('Navigator'),
+//               ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,87 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_nike_shoe_store/login_screen.dart';
+
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+
+  @override
+  void initState() {
+    super.initState();
+
+
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+    });
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [Color(0xff6DADFC), Color(0xff0188E9)],
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(right: 30),
+              child: Image.asset('assets/images/blue2.png'
+                  // width: 300,
+                  ),
+            ),
+            Text(
+              'Nike Store',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'www.nike.com',
+              style: TextStyle(color: Colors.white, fontSize: 22),
+            ),
+            // ElevatedButton(
+            //   onPressed: () {
+            //     // Navigator.of(context).push(MaterialPageRoute(builder: (context) => ),);
+            //
+            //
+            //     Navigator.of(context).push(
+            //       MaterialPageRoute(
+            //         builder: (context) => LoginScreen(),
+            //       ),
+            //     );
+            //
+            //
+            //
+            //
+            //   },
+            //   child: Text('Navigator'),
+            // ),
+          ],
+        ),
+      ),
+    );
+  }
+}
